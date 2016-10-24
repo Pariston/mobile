29b50ef15ee267c7456b294ef11a87e7eea62c67
Daniel Bojarski <lawlietdb@gmail.com>
2016-10-25 01:42:42 +0200

    Zaklecie

---
+++ BBC2/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using BBC2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BBC2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        dbconnection db = new dbconnection();
+        private List<Article> poziomki;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            poziomki = db.getArticles();
+        }
+
+        private void archivebutton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(archive));
+        }
+    }
+}
---
+++ BBC2/Models/dbconnection.cs
@@ -0,0 +1,27 @@
+﻿using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBC2.Models
+{
+    class dbconnection
+    {
+        SQLiteConnection conn;
+
+        public dbconnection()
+        {
+            string path = Path.Combine(Windows.ApplicationModel.Package.Current.InstalledLocation.Path, "db", "bbc.db");
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+        }
+
+        public List<Article> getArticles()
+        {
+            var data = conn.Table<Article>();
+            return data.ToList();
+        }
+    }
+}
